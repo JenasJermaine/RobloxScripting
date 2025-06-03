7af0c69dcddb58359a5fc3430a5aacde6076abe4
Jenas Jermaine <barakajenas@gmail.com>
2025-06-03 10:51:31 +0300

    if statements

---
+++ ifstatements.lua
@@ -0,0 +1,22 @@
+condition= true
+condition2= nil
+condition3= 10
+
+
+if condition then
+    print("The condition was truth")
+end
+
+if condition2 then
+    print("The condition was truth")
+end
+
+if condition3 then
+    print("The condition was truth")
+end
+
+if condition3 > 10 then
+    print("The condition was truth")
+else
+    print("The condition was falsey")
+end
