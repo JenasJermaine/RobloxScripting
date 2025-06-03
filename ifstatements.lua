1cf0024e2dba42db45f5447b6cec339c8efe8bb9
Jenas Jermaine <barakajenas@gmail.com>
2025-06-03 12:11:46 +0300

    lexical scope

--- ifstatements.lua
+++ ifstatements.lua
@@ -1,6 +1,7 @@
 condition= true
 condition2= nil
 condition3= 10
+condition4= 30
 
 
 if condition then
@@ -19,4 +20,23 @@ if condition3 > 10 then
     print("The condition was truth")
 else
     print("The condition was falsey")
+end
+
+if condition3 > 10 then
+    print("condition3 was truthy")
+elseif condition4 ~= 30 then
+    print("condition4 was truthy")
+else
+    print("All conditions were falsey")
+end
+
+weather = "overcast"
+if weather == "snowing" then
+	print("Today is a cold day")
+elseif weather == "raining" then
+	print("Grab an umbrella, it's raining!")
+elseif weather == "sunny" then
+	print("Time for a tan!")
+else
+	print("It's getting cloudy maybe it might rain")
 end
---
+++ lexicalscope.lua
@@ -0,0 +1,16 @@
+local whereAmI= "Bathroom"
+if whereAmI == "Bathroom" then
+    local knife= "sharp"
+elseif whereAmI =="Kitchen" then
+    local toilet = "Flush"
+end
+
+whereAmI = "Bathroom"
+if whereAmI == "Bathroom" then
+    local whereAmI = "Toilet"
+    print(whereAmI)
+    local toilet = "Flush"
+elseif whereAmI == "Kitchen" then
+    local knife = "sharp"
+end
+print(whereAmI)
