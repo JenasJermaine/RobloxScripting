c4fba648b075ed6d9661a5fdf689986c133663fa
Jenas Jermaine <barakajenas@gmail.com>
2025-06-03 04:51:32 +0300

    first commit

---
+++ variables.lua
@@ -0,0 +1,29 @@
+myFirstVar= 10
+--[[
+Seven basic data types in Lua:
+1. nil - Represents a lack of value.
+2. boolean - Represents true or false.
+3. number - Represents numeric values (integers or floats).
+4. string - Represents a sequence of characters.
+5. function - Represents a callable function.
+6. table - Represents a collection of key-value pairs (similar to arrays or dictionaries).
+7. thread - Represents a separate thread of execution (used in coroutines).
+]]
+quoteOfTheDay = "Don't be a lazy coder!"
+quoteOfTheDay =
+[[
+Don't be a
+lazy coder
+]]
+booleanExample = true
+numberExample = 1.923
+nilExample = nil
+
+quoteOfTheDay = 100 -- overwriting the previous value and data type
+
+print(quoteOfTheDay)
+print(booleanExample)
+print(numberExample)
+print(nilExample)
+
+
